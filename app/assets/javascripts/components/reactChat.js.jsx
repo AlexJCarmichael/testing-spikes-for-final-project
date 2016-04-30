522e06df2effa6e021228da2efcaaa17882efb76
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-30 14:09:28 -0500

    Build react component for holding chat messages.

---
+++ app/assets/javascripts/components/reactChat.js.jsx
@@ -0,0 +1,41 @@
+var CharHolder = React.createClass({
+  getInitialState: function () {
+    return {
+      messages: []
+    };
+  },
+
+  tick: function() {
+    var that = this;
+    var url = document.URL;
+    $.getJSON(url, function(response){
+      that.setState({
+        messages: response.messages
+      })
+    });
+  },
+
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  render: function() {
+    return (
+      <div>
+      {this.state.comments.map(function(comment){
+        return (
+          <MessageDisplay
+            key={message.id}
+            user_name={current_user.email}
+            text={message.text}
+            />
+        );
+      })}
+      </div>
+    );
+  }
+});
